801d160aaa3a499534cf9d5cbdb44a2128b912b6
ezequielnaftali <enaftali@pisol.net>
2022-08-18 15:30:57 -0300

    version completa

--- main.php
+++ main.php
@@ -2,65 +2,92 @@
 
 require "clsAPI.php";
  
-$config = array('endpoint'=>'https://api.stagingeb.com/v1/', 'token'=>'05mh61963tpe2bi7efwoamhciwt0h5');
-//$prop = APICall::get($config, 'properties',array());
-//var_dump($prop->content);
-echo "\n";
-$p = new Property('titulo', ' locacion', 400000, 'USD');
-echo $p->getDescription();
-echo "\n fin \n ";
+class PropertyRepositoryAPI implements getPropertyInterface{
 
-class Property{
-
-  private $title;
-  private $location;
-  private $price;
-  private $currency;
-  public function __construct($title, $location, $price, $currency){
-    $this->title = $title;
-    $this->location = $location;
-    $this->price = $price;
-    $this->currency = $currency;
+  private $config;  //API configuration object
+  public function __construct($configObject){
+    $this->config = $configObject;    
   }
-  public function getTitle()
+  public function getAll()
   {
-    return $this->title;    
+    $prop = APICall::get($this->config, 'properties',array()); //in the future implement other parameters like pagination   
+    return $prop;
   }
-  public function getLocation(){
-    return $this->location;
+  public function getOne($id){
+   //TBD in the future
   }
-  public function getPrice(){ 
-    $price = $this->price." ".$this->currency;
-    return $price;
-  }
-  public function getDescription(){
-    return ($this->getTitle(). " " . $this->getLocation(). " " . $this->getPrice());
-  }
-
 }
 
-abstract class Fruit {
-  public $name;
-  public $color;
-  public function __construct($name, $color) {
-    $this->name = $name;
-    $this->color = $color;
+
+
+class configManager{
+  private $endpoint;
+  private $token;
+  //in the future this can be overloaded to get config info from better sources
+  public function __construct($api_url, $token){
+        $this->endpoint = $api_url;
+        $this->token = $token;
+  }
+  public function getConfig() 
+  {
     
+    $config = array('endpoint'=> "".$this->endpoint."");
+    $config += array('token'=> "".$this->token."");
+    return $config;
   }
-  public function intro() {
-    echo "The fruit is {$this->name} and the colour is {$this->color}.";
-  }
-  public abstract function message();
+
 }
 
-// Strawberry is inherited from Fruit
-class Strawberry extends Fruit {
-  public function message() {
-    echo "Am I a fruit or a berry? ";
-  }
+
+interface getPropertyInterface{
+  public function getAll();
+  public function getOne($id);
+  
 }
 
-//$strawberry = new Strawberry("Strawberry", "red");
-//$strawberry->message();
-//$strawberry->intro();
+
+
+ class PropertyController
+ {
+     private $repository;
+ 
+     public function __construct(getPropertyInterface $repository)
+     {
+         $this->repository = $repository;    
+     } 
+     public function getAll() // returns all the properties from repository call
+     {
+         $properties_data = $this->repository->getAll();
+         $propertiesList = array();
+         foreach($properties_data->content as $key => $val) {
+          $propertiesList[] = new Property($val->title,$val->location,$val->operations[0]->amount,$val->operations[0]->currency);
+
+         }
+         return $propertiesList;
+     }
+     public function renderPropertiesList($listOfProperties){
+        foreach($listOfProperties as $prop){          
+          echo $prop->getDescription()."\n";  //console print the property 
+        }
+
+     }
+ }
+
+
+
+
+
+
+$configManager =  new configManager ('https://api.stagingeb.com/v1/','05mh61963tpe2bi7efwoamhciwt0h5');
+$config = $configManager->getconfig();
+
+$propertyRepository = new PropertyRepositoryAPI($config);
+$propertyController = new PropertyController($propertyRepository);
+$propertyList = $propertyController->getAll();
+$propertyController->renderPropertiesList($propertyList);
+echo "\n  \n ";
+
+
+
+
 ?>
